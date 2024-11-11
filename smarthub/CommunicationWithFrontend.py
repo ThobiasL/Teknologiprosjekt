df1656e9dcf7d3d23db2cef038ab56a6db62d384
Thobias <thobiasl@hiof.no>
2024-11-11 17:02:14 +0100

    opprettet .py filer som skal være API-er
    CommunicationWithExternalDevices
    CommunicationWithDatabase
    CommunicationWithFrontend

---
+++ smarthub/CommunicationWithFrontend.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+class FrontendCommunication:
+    def __init__(self, signal):
+        self.signal = signal
+
+    def getInfoFromFrontend(self):
+        if
+          return
+        return None
+
+    def sendInfoToFrontend(self, signal):
+        if
+
+
+
