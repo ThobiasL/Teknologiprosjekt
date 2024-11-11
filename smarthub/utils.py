8736b7c1bf64f21bb2bf6dedc03f9917d7592def
Thobias <thobiasl@hiof.no>
2024-11-11 17:27:52 +0100

    opprydding av mapper og flytting av filer

--- smarthub/utils.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-# Sjekker om tid følger formatet HH:MM, og returnerer False hvis ikke
-def is_valid_time(value):
-    try:
-        datetime.strptime(value, '%H:%M')
-        return True
-    except ValueError:
-        return False
