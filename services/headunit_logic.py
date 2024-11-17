3bcf51592c2584ac27758a092bf63b6776b8a9dd
Mathias <mathiasbthorsen@gmail.com>
2024-11-17 23:48:59 +0100

    a

--- services/headunit_logic.py
+++ services/headunit_logic.py
@@ -1,25 +1,16 @@
 # services/headunit_logic.py
-
 from services.database_operations import DatabaseOperations
 
 class HeadunitLogic:
-    def __init__(self):
-        self.db_ops = DatabaseOperations()
-
     def readVariableStatusFromDatabase(self):
-        return self.db_ops.get_auto_door_lock_status()
+        return DatabaseOperations.get_auto_door_lock_status()
 
     def sendAutoDoorLockTimeToDatabase(self, status):
-        self.db_ops.set_auto_door_lock_status(status)
-
-    def readMedicationDosesFromDatabase(self, day):
-        return self.db_ops.get_medication_doses(day)
-
-    def sendMedicationDosesStatusToDatabase(self, medication_id, dose_id):
-        self.db_ops.set_medication_dose_status(medication_id, dose_id)
+        DatabaseOperations.set_auto_door_lock_status(status)
 
     def readTasksFromDatabase(self):
-        return self.db_ops.get_all_tasks()
+        tasks = DatabaseOperations.get_all_tasks()
+        return [{"name": task.get('name'), "time": task.get('time')} for task in tasks]
 
     def taskDone(self, name, time):
-        self.db_ops.mark_task_done(name, time)
+        DatabaseOperations.mark_task_done(name, time)
